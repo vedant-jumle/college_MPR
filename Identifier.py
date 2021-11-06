a5ab24b2a496d4b581510e9f44a30c961d8480ff
maxDeCoder <vedantjumle@gmail.com>
2021-11-06 21:52:47 +0530

    First Commit

---
+++ Identifier.py
@@ -0,0 +1,23 @@
+from tensorflow import keras
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from PIL import Image
+import json
+
+class Identifier:
+    def __init__(self, model_path="./models/model_v3_b20_trainable_conv.model", classes_path="classes.json", image_dims=(256,256)):
+        self.classes_map = json.load(open(classes_path, "r"))
+        self.classes_list = list(self.classes_map.keys())
+        self.model = keras.models.load_model(model_path)
+        self.IMAGE_DIMS = image_dims
+
+    def predict_image(self, IMAGE_PATH):
+        img = image.load_img(IMAGE_PATH, target_size=(self.IMAGE_DIMS), color_mode='rgb')
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)/255
+        images = np.vstack([x])
+        test = self.model(images)
+        p_class = np.argmax(test.numpy()[0])
+        probabilty = test[0][p_class].numpy()
+        return self.classes_map[self.classes_list[p_class]], round(probabilty*100, 2)
---
+++ server.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from Identifier import Identifier
+import os
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+identifier = Identifier()
+UPLOAD_FOLDER = 'static'
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/img/<filename>")
+def return_img(filename):
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+@app.route('/', methods=['POST'])
+def upload_image():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#print('upload_image filename: ' + filename)
+		flash('Image successfully uploaded and displayed below')
+		return redirect("/predict/")
+	else:
+		flash('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect(request.url)
+
+app.route("/predict/<filename>")
+def predict(filename):
+    pass
